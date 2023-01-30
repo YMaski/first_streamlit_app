dcff8bf2029411d18a3942b1c3bae82b1b31a098
Yugandhar Maski <101830365+YMaski@users.noreply.github.com>
2023-01-30 11:52:01 +0000

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -46,8 +46,8 @@ def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
           my_cur.execute("select * from fruit_load_list")
           return mycur.fetchall()
-     
- if streamlit.button('Get Fruit load List'):
+
+if streamlit.button('Get Fruit load List'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      my_data_rows = get_fruit_load_list()
      streamlit.dataframe(my_data_rows)
@@ -55,8 +55,6 @@ def get_fruit_load_list():
 streamlit.stop()
 
 #import snowflake.connector
-
-
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
